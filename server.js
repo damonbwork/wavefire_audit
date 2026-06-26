ec9c839c125f825c763b39336ade7b5bd6a007a8
damonbwork <damonbwork@yahoo.com>
2026-06-26 11:35:48 -0500

    Add HTTPS module to server.js

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ require('dotenv').config();
 const express = require('express');
 const cors    = require('cors');
 const path    = require('path');
+const https   = require('https');   
 
 const app  = express();
 const PORT = process.env.PORT || 3000;
